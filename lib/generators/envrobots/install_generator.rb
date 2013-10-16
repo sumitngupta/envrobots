0682e03e02b39192b1f8a1fddb4cfce331a631dd
James Martinez <jamescmartinez@gmail.com>
2013-10-16 00:35:56 -0700

    different robot.txt depending on env

--- lib/generators/envrobots/install_generator.rb
+++ lib/generators/envrobots/install_generator.rb
@@ -4,7 +4,8 @@ module Envrobots
       source_root File.expand_path('../../templates', __FILE__)
 
       def copy_robots
-        copy_file 'robots.txt', 'config/robots/robots.production.txt'
+        copy_file 'robots.production.txt', 'config/robots/robots.production.txt'
+        copy_file 'robots.other.txt', 'config/robots/robots.other.txt'
       end
     end
   end
