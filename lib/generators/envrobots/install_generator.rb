d20e88624c3f2c2a88463aebe5174d0ff1819ba2
James Martinez <jamescmartinez@gmail.com>
2013-10-16 03:35:36 -0700

    install generator now checks for existing robots.txt

--- lib/generators/envrobots/install_generator.rb
+++ lib/generators/envrobots/install_generator.rb
@@ -4,7 +4,13 @@ module Envrobots
       source_root File.expand_path('../../templates', __FILE__)
 
       def copy_robots
-        copy_file 'robots.production.txt', 'config/robots/robots.production.txt'
+        path = Rails.root.join('public', 'robots.txt')
+        if File.file? path
+          copy_file path, 'config/robots/robots.production.txt'
+          remove_file path
+        else
+          copy_file 'robots.production.txt', 'config/robots/robots.production.txt'
+        end
         copy_file 'robots.other.txt', 'config/robots/robots.other.txt'
       end
     end
