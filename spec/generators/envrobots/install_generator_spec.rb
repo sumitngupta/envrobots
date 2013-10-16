350bcb71700324efb2085064ab7a6cfd6e20709a
James Martinez <jamescmartinez@gmail.com>
2013-10-16 00:25:36 -0700

    generator spec in progress

---
+++ spec/generators/envrobots/install_generator_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Envrobots::Generators::InstallGenerator do
+
+  describe '#copy_robots' do
+    it 'copys the robots.txt file from the template to the config dir'
+  end
+
+end
